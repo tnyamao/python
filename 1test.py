12eab1f89c10ee4113013a4ae60ea59c134c957a
tyamazaki <t-yamazaki@chishiki-k.co.jp>
2020-08-26 23:38:28 +0900

    first_commit

---
+++ 1test.py
@@ -0,0 +1,10 @@
+def search(data, target):
+    for i in range(len(data)):
+        print("添え字: ",i)
+        print("検査値: ",target)
+        if data[i] == target:
+            return i
+        return -1
+data = [1,2,3,4,5,6,7,8,9]
+target = 7
+print("要素番号{}にデータ{}を見つけました。".format(search(data, target), target))
